37b35207bb6b7b7cffe9a0444b947e3d9c5af3ba
LIJUNYU-94 <24aw0141@jec.ac.jp>
2025-01-20 23:51:26 +0900

    1

--- app/songs/page.tsx
+++ app/songs/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import Menu from "../components/menu";
 import { SongProvider, useSong } from "../components/SongContext";
-import data from "../../src/mousouslave.json";
+// import data from "../../src/mousouslave.json";
 
 function SongsContent() {
-  const { selectedSongName } = useSong();
+  // const { selectedSongName } = useSong();
   // const lyrics = selectedSongName
   //   ? data.find((song) => song.name === selectedSongName)?.lyrics ||
   //     "歌詞が見つかりません"
