7e93eea66f5a84fe8c35b4cfb64bb63eae5de1a9
LIJUNYU-94 <24aw0141@jec.ac.jp>
2025-02-09 02:01:31 +0900

    1

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
   const smallscreen = SmallScreen();
   return (
     <>
-      <main className="h-[100dvh] bg-slate-400/80 max-w-[500px]">
+      <main className="h-[100dvh] bg-slate-400/80 max-w-[500px] mx-auto">
         <h1 className="text-4xl font-bold text-center py-[10dvh] text-white bg-[url('/header.png')] bg-cover bg-center">
           妄想slave <br />
           非公式応援サイト
